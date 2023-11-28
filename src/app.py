7598bc14659dba552bc04fbcd536f782337ba947
Mike Bell <mike.bell1@digital.justice.gov.uk>
2023-11-28 14:20:48 +0000

    Add healthz endpoints and bump container version to 1.1.0

--- src/app.py
+++ src/app.py
@@ -1,9 +1,30 @@
 from flask import Flask
 from prometheus_client import Counter, generate_latest
+from flask_healthz import healthz, HealthError
 
 app = Flask(__name__)
+app.register_blueprint(healthz, url_prefix="/healthz")
 
 view_metric = Counter('view', 'Home view')
+
+def liveness():
+    try:
+        print("OK")
+    except Exception:
+        raise HealthError("Liveness probe fail")
+
+def readiness():
+    try:
+        print("OK")
+    except Exception:
+        raise HealthError("Readiness probe fail")
+
+app.config.update(
+    HEALTHZ = {
+        "live": "app.liveness",
+        "ready": "app.readiness",
+    }
+)
 
 @app.route("/")
 def hello_world():
